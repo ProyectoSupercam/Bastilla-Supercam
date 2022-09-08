351645b1652f538f3dd549f8c0de58958bb663d2
Supercam <proyectos.supercam@gmail.com>
2022-09-08 16:43:23 -0300

    creacion del modulo  usuario para iniciar sesión

--- Backend/src/database/database.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import  config  from '../ormconfig';
-import {  TypeOrmModule } from "@nestjs/typeorm";
-
-@Module({
-  imports:[TypeOrmModule.forRoot(config)]
-})
-export class DatabaseModule {}
